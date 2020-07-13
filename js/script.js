e90b50ab7b07ec5ae161ea53f8473b8547ca7b8f
Tresor Mazinga <mazingatresor3@gmail.com>
2020-07-13 19:54:30 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// WEBSITE LOADER JS
+
+document.querySelector('.main').style.display = 'none';
+document.getElementById('load').classList.add('loader');
+//
+setTimeout(() => {
+    document.getElementById('load').classList.remove('loader');
+    document.querySelector('.main').style.display = 'block';
+
+}, 4000);
+//================ WEBSITE LOADER =============================
